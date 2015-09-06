90b379f1f9f40a91fe2f397a167606d0372fa6f3
Ubuntu <ubuntu@localhost.localdomain>
2015-09-06 19:57:05 +0530

    commited after running topic extraction program

--- ExtractTopics.py
+++ ExtractTopics.py
@@ -3,21 +3,17 @@ from gensim.models import TfidfModel, LdaModel
 from gensim.utils import smart_open, simple_preprocess
 from gensim.corpora.wikicorpus import _extract_pages, filter_wiki
 from gensim import corpora
-
+from gensim.models.ldamulticore import LdaMulticore
 
 wiki_corpus = MmCorpus('Wiki_Corpus.mm')   # Loading the corpus 
 print (".... successfully loaded the corpus")
 
-wiki_dict = Dictionary.load('WikiDictionary.dict') # Loading the dictionary
+wiki_dict = Dictionary.load('WikiDictionary200k.dict') # Loading the dictionary
 print (".... successfully loaded the dictionary")
 
-print wiki_corpus
+lda = LdaMulticore(corpus=wiki_corpus, id2word=wiki_dict, num_topics=300, chunksize=10000, passes=2)
 
+print ".... successfully extracted the topics; saving the model"
+lda.save('WikiLDA_300.lda')
 
-
-# lda = LdaModel(corpus=wiki_corpus, id2word=wiki_dict, num_topics=400, update_every=1, chunksize=10000, passes=2)
-
-# print ".... successfully extracted the topics; saving the model"
-# lda.save('WikiLDA_400.lda')
-
-# print "finished ...."
+print "finished ...."
